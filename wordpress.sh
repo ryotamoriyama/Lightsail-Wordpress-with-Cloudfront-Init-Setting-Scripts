fc869ed8664184928d5a4cc4065119894104759f
ryota <moriyama_ryota@liginc.co.jp>
2019-03-03 08:51:25 +0900

    Escape

--- wordpress.sh
+++ wordpress.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #site_url・home_url変更
-sed -i -e "s|'http:\/\/' . $_SERVER['HTTP_HOST'] . '\/'|'https:\/\/${1}\/‘|g" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
+sed -i -e "s|^define('WP_SITEURL', 'http:\/\/' . \$_SERVER\['HTTP_HOST'\] . '\/');|define('WP_SITEURL', 'https:\/\/${1}\/‘)|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
+sed -i -e "s|^define('WP_HOME', 'http:\/\/' . \$_SERVER\['HTTP_HOST'\] . '\/');|define('WP_SITEURL', 'https:\/\/${1}\/‘)|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
 
 #$_SERVER['HTTP_HOST']指定
 sed -i -e "97s|^|\n$_SERVER['HTTP_HOST'] = ‘${1}’;\n\n|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
