5b198bb7368a07f325014c3765f956c91c08d2f3
ryota <moriyama_ryota@liginc.co.jp>
2019-03-03 08:38:50 +0900

    Modify sed

--- wordpress.sh
+++ wordpress.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #site_url・home_url変更
-sed -i -e "s|define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/‘);|define('WP_SITEURL', 'https://${1}/‘)|" /opt/bitnami/apache2/conf/httpd.conf
-sed -i -e "s|define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/‘);|define('WP_SITEURL', 'https://${1}/‘)|" /opt/bitnami/apache2/conf/httpd.conf
+sed -i -e "s|^define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/');|define('WP_SITEURL', 'https://${1}/‘)|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
+sed -i -e "s|^define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/');|define('WP_SITEURL', 'https://${1}/‘)|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
 
 #$_SERVER['HTTP_HOST']指定
-sed -i -e "97s|^|$_SERVER['HTTP_HOST'] = ‘${1}’;|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
+sed -i -e "97s|^|\n$_SERVER['HTTP_HOST'] = ‘${1}’;\n\n|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
 
 #プロトコルをCloudfrontに合わせる
-sed -i -e "101s|^|\nif (isset($SERVER['HTTP_X_FORWARDED_PROTO']) && $SERVER['HTTPX_FORWARDEDPROTO'] === 'https') {\n\t$_SERVER['HTTPS'] = 'on';\n}\n|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
+sed -i -e "101s|^|\nif (isset($SERVER['HTTP_X_FORWARDED_PROTO']) && $SERVER['HTTPX_FORWARDEDPROTO'] === 'https') {\n\t$_SERVER['HTTPS'] = 'on';\n}\n\n|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
