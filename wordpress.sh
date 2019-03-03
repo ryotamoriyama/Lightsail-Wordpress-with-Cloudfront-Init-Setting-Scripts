ed9a48d66d3c060f5dbca262b8fd4c78192192f9
ryota <moriyama_ryota@liginc.co.jp>
2019-03-03 09:17:30 +0900

    Add CLI commands

--- wordpress.sh
+++ wordpress.sh
@@ -9,3 +9,17 @@ sed -i -e "97s|^|\n$_SERVER['HTTP_HOST'] = ‘${1}’;\n\n|" /opt/bitnami/apps/w
 
 #プロトコルをCloudfrontに合わせる
 sed -i -e "101s|^|\nif (isset($SERVER['HTTP_X_FORWARDED_PROTO']) && $SERVER['HTTPX_FORWARDEDPROTO'] === 'https') {\n\t$_SERVER['HTTPS'] = 'on';\n}\n\n|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
+
+#WP CLI
+cd /opt/bitnami/wordpress/htdocs
+
+#WordPressを最新版にアップデート
+sudo wp core update --allow-root
+
+#プラグイン削除
+wp plugin delete $(wp plugin list --status=inactive --field=name)
+
+#テーマ削除
+wp theme delete $(wp theme list --status=inactive --field=name)
+active=$(wp theme list --status=active --field=name)
+rm -Rf "/opt/bitnami/apps/wordpress/htdocs/wp-content/themes/${active}"
