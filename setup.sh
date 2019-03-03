c17d0494b5994ef0b27d9d1a1eebd18a81bc4ab4
ryota <moriyama_ryota@liginc.co.jp>
2019-03-03 16:38:40 +0900

    test

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ cat /opt/bitnami/apps/Lightsail-Wordpress-with-Cloudfront-Init-Setting-Scripts/l
 sudo /opt/bitnami/ctlscript.sh start
 
 #wordpress設定
-sudo bash /opt/bitnami/apps/Lightsail-Wordpress-with-Cloudfront-Init-Setting-Scripts/wordpress.sh ${1}
+sudo bash /opt/bitnami/apps/Lightsail-Wordpress-with-Cloudfront-Init-Setting-Scripts/wordpress.sh ${1} ${2} ${3}
 
 sudo apt install -y awscli
 sudo apt install -y expect
