6cda360905c05a491cf18ae363d22ecd5a88989a
ryota <moriyama_ryota@liginc.co.jp>
2019-03-03 10:14:28 +0900

    test

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -8,7 +8,7 @@ cd /tmp
 curl -s https://api.github.com/repos/xenolf/lego/releases/latest | grep browser_download_url | grep linux_amd64 | cut -d '"' -f 4 | wget -i -
 
 for filename in /tmp/lego_v*_linux_amd64.tar.gz; do
-  last=${filename#./lib/lego_v*}
+  last=${filename#/tmp/lego_v*}
   version=${last%%_*}
   if [ -n "$version" ]; then
   break
