c3e86cdd48409b7268ee0da8598f85df30983633
ryota <moriyama_ryota@liginc.co.jp>
2019-03-03 20:02:13 +0900

    Add let's encrypt script

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -23,20 +23,20 @@ tar xf "/tmp/lego_v${version}_linux_amd64.tar.gz"
 sudo mv lego /usr/local/bin/lego
 
 #Apacheにインストール
-sudo lego --email="${3}" --domains="${2}" --path="/etc/lego" --tls run
+sudo lego --email="${2}" --domains="${1}" --path="/etc/lego" --tls run
 
 sudo mv /opt/bitnami/apache2/conf/server.crt /opt/bitnami/apache2/conf/server.crt.old
 sudo mv /opt/bitnami/apache2/conf/server.key /opt/bitnami/apache2/conf/server.key.old
 sudo mv /opt/bitnami/apache2/conf/server.csr /opt/bitnami/apache2/conf/server.csr.old
-sudo ln -s "/etc/lego/certificates/${2}.key" /opt/bitnami/apache2/conf/server.key
-sudo ln -s "/etc/lego/certificates/${2}.crt" /opt/bitnami/apache2/conf/server.crt
+sudo ln -s "/etc/lego/certificates/${1}.key" /opt/bitnami/apache2/conf/server.key
+sudo ln -s "/etc/lego/certificates/${1}.crt" /opt/bitnami/apache2/conf/server.crt
 sudo chown root:root /opt/bitnami/apache2/conf/server*
 sudo chmod 600 /opt/bitnami/apache2/conf/server*
 
 #証明書更新用スクリプトの作成
 sudo cp -f /opt/bitnami/apps/Lightsail-Wordpress-with-Cloudfront-Init-Setting-Scripts/lib/renew-certificate.sh /etc/lego/renew-certificate.sh
-sudo sed -i -e "s|{replace-email}|${3}|" /etc/lego/renew-certificate.sh
-sudo sed -i -e "s|{replace-domains}|${2}|" /etc/lego/renew-certificate.sh
+sudo sed -i -e "s|{replace-email}|${2}|" /etc/lego/renew-certificate.sh
+sudo sed -i -e "s|{replace-domains}|${1}|" /etc/lego/renew-certificate.sh
 
 #crontab登録
 sudo rm -Rf /var/spool/cron/crontabs/root
--- setup.sh
+++ setup.sh
@@ -31,3 +31,6 @@ sudo bash /opt/bitnami/apps/Lightsail-Wordpress-with-Cloudfront-Init-Setting-Scr
 
 sudo apt install -y awscli
 sudo apt install -y expect
+
+#Let's encryptインストール
+sudo bash /opt/bitnami/apps/Lightsail-Wordpress-with-Cloudfront-Init-Setting-Scripts/letsencrypt.sh ${2} ${1}
