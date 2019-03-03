934d72eb1252be0b002f09db54b0d6018f1f5aad
ryota <moriyama_ryota@liginc.co.jp>
2019-03-03 14:55:10 +0900

    Modify quote

--- wordpress.sh
+++ wordpress.sh
@@ -5,7 +5,7 @@ sed -i -e "s|^define('WP_SITEURL', 'http:\/\/' . \$_SERVER\['HTTP_HOST'\] . '\/'
 sed -i -e "s|^define('WP_HOME', 'http:\/\/' . \$_SERVER\['HTTP_HOST'\] . '\/');|define('WP_SITEURL', 'https:\/\/${1}\/');|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
 
 #$_SERVER['HTTP_HOST']指定
-sed -i -e "97s|^|\n\$_SERVER['HTTP_HOST'] = ‘${1}’;\n\n|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
+sed -i -e "97s|^|\n\$_SERVER['HTTP_HOST'] = '${1}';\n\n|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
 
 #プロトコルをCloudfrontに合わせる
 sed -i -e "101s|^|\nif (isset(\$_SERVER['HTTP_X_FORWARDED_PROTO']) && \$_SERVER['HTTPX_FORWARDEDPROTO'] === 'https') {\n\t\$_SERVER['HTTPS'] = 'on';\n}\n\n|" /opt/bitnami/apps/wordpress/htdocs/wp-config.php
